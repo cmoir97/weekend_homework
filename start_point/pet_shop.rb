b0c3976f5661e75d283d26ba4f7161deb225d080
Cameron Moir <cameronjmoir@gmail.com>
2019-09-13 11:54:03 +0100

    second test completed

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -1,3 +1,7 @@
-def pet_shop_name(array)
-  return array[:name] 
+def pet_shop_name(pet_shop_array)
+  return pet_shop_array[:name]
+end
+
+def total_cash(pet_shop_array)
+  return pet_shop_array[:admin][:total_cash]
 end
