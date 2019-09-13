8fff1f4b4d85e7c7158d656a685330516abeea93
Cameron Moir <cameronjmoir@gmail.com>
2019-09-13 12:00:04 +0100

    third test completed

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -5,3 +5,7 @@ end
 def total_cash(pet_shop_array)
   return pet_shop_array[:admin][:total_cash]
 end
+
+def add_or_remove_cash(pet_shop_array, cash_to_add)
+ return pet_shop_array[:admin][:total_cash] += cash_to_add
+end
